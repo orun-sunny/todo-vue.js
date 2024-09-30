dc81cdd1c045362b508f168af5228cc9a5ee1f4f
orun-sunny <sunny.mukut@gmail.com>
2024-09-30 18:43:54 +0600

    workig on routing

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,13 @@
 </template>
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  mounted() {
+    let user = localStorage.getItem('user-info')
+    if (!user) {
+      this.$router.push({ name: 'SignUp' })
+    }
+  }
 }
 </script>
 <style lang=""></style>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -33,9 +33,9 @@ export default {
       console.warn(result)
       if (result.status == 201) {
         // alert('Sign up Done')
+        localStorage.setItem('user-info', JSON.stringify(result.data))
+        this.$router.push({ name: 'Home' })
       }
-      localStorage.setItem('user-info', JSON.stringify(result.data))
-      this.$router.push({ name: 'Home' })
     }
   }
 }
